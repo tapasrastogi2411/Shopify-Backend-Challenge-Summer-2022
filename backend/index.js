c740570047a893c0726b0f92acf3e56733df7687
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2022-01-17 12:41:27 -0500

    Added code to connect to a mongoDB db

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 // index.js is the entry point of our server
 
 // Setting up our express server 
-const express = require('express')
+const express = require('express');
+
+// Importing and setting up our MongoDB database
+const mongoose = require('mongoose')
 
 // Initialising our express server
-const app = express
+const app = express();
 
+// Code to allow us to receive information from the frontend in JSON format, necessary to get all of our information correctly
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://newUser-1:vaangevaange1@iventory-crud.aek0k.mongodb.net/inventoryTracking?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+});
+
+// Starting the server
 app.listen(3001, () => {
     console.log('Server running on port 3001...')
 });
