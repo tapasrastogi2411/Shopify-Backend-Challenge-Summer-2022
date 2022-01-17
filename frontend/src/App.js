4cf6754def5914ed7bf4c2e3525eefbcca55018f
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2022-01-17 16:18:29 -0500

    Added useStates to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,55 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // Creating useStates for the CRUD operations
+  const [itemName, setItemName] = useState('')
+  const [brandName, setBrandName] = useState('')
+  const [itemDescription, setItemDescription] = useState('')
+  const [quantity, setQuantity] = useState(0)
+  const [price, setPrice] = useState(0)
+
+
   return (
     <div className="App">
       <h1> Inventory Manager</h1>
 
 
       <label>Item Name </label>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(event) => {
+          setItemName(event.target.value);
+        }} />
 
       <label>Brand </label>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(event) => {
+          setBrandName(event.target.value);
+        }} />
 
       <label>Item Description</label>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(event) => {
+          setItemDescription(event.target.value);
+        }} />
 
       <label>Quantity</label>
-      <input type="number" />
+      <input
+        type="number"
+        onChange={(event) => {
+          setQuantity(event.target.value);
+        }} />
 
       <label>Price</label>
-      <input type="number" />
+      <input
+        type="number"
+        onChange={(event) => {
+          setPrice(event.target.value);
+        }} />
 
       <button> Add new item</button>
 
