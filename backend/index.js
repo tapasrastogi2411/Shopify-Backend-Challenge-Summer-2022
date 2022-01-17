e21702fb2d3f76115aa55e9c7d06c07014eca2d2
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2022-01-17 15:01:46 -0500

    Added code which adds data points into the MongoDB database

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,33 @@
 // index.js is the entry point of our server
 
-// Setting up our express server 
-const express = require('express');
+const express = require('express'); // Setting up our express server 
+const mongoose = require('mongoose') // Importing and setting up our MongoDB database
+const app = express(); // Initialising our express server
 
-// Importing and setting up our MongoDB database
-const mongoose = require('mongoose')
-
-// Initialising our express server
-const app = express();
+const itemModel = require("./schemas/itemSchema") // Importing the schema into this file
 
 // Code to allow us to receive information from the frontend in JSON format, necessary to get all of our information correctly
 app.use(express.json())
 
+// Adding the connection to our MongoDB database
 mongoose.connect("mongodb+srv://newUser-1:vaangevaange1@iventory-crud.aek0k.mongodb.net/inventoryTracking?retryWrites=true&w=majority", {
     useNewUrlParser: true,
 });
+
+// Creating a route - Whenever someone comes across this route, I want to enter/create an item in the database
+app.get('/', async (req, res) => {
+
+    const item = new itemModel({Name: "T-shirt", Brand: "Adidas", Description: "Unisex S - Red T-shirt", Quantity: 2, Price: 250});
+
+    try {
+
+        await item.save(); // Telling the database to save this information in the server
+        res.send("Inserted data entry!")
+    } catch(err){
+        console.log(err)
+    }
+
+})
 
 // Starting the server
 app.listen(3001, () => {
