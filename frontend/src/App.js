3b7e9a90356e4e76b3bb624dcb7fb1be9c686287
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2022-01-17 15:56:24 -0500

    Added basic html text boxes and buttons for the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1> Inventory Manager</h1>
+
+
+      <label>Item Name </label>
+      <input type="text" />
+
+      <label>Brand </label>
+      <input type="text" />
+
+      <label>Item Description</label>
+      <input type="text" />
+
+      <label>Quantity</label>
+      <input type="number" />
+
+      <label>Price</label>
+      <input type="number" />
+
+      <button> Add new inventory item</button>
+
     </div>
   );
 }
