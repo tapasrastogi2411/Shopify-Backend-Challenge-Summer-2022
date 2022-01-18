4a5cb14e81fc17b65dd7675d9a86ea351bf86ab9
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2022-01-17 19:18:13 -0500

    Finished the Create-POST request route to add an item to the database

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@
 
 const express = require('express'); // Setting up our express server 
 const mongoose = require('mongoose') // Importing and setting up our MongoDB database
+const cors = require('cors') // To interact with the APIs that we make
 const app = express(); // Initialising our express server
 
 const itemModel = require("./schemas/itemSchema") // Importing the schema into this file
 
 // Code to allow us to receive information from the frontend in JSON format, necessary to get all of our information correctly
 app.use(express.json())
+app.use(cors())
 
 // Adding the connection to our MongoDB database
 mongoose.connect("mongodb+srv://newUser-1:vaangevaange1@iventory-crud.aek0k.mongodb.net/inventoryTracking?retryWrites=true&w=majority", {
@@ -15,21 +17,27 @@ mongoose.connect("mongodb+srv://newUser-1:vaangevaange1@iventory-crud.aek0k.mong
 });
 
 // Creating a route - Whenever someone comes across this route, I want to enter/create an item in the database
-app.get('/', async (req, res) => {
+app.post('/insert', async (req, res) => {
 
-    const item = new itemModel({Name: "T-shirt", Brand: "Adidas", Description: "Unisex S - Red T-shirt", Quantity: 2, Price: 250});
+    const itemName = req.body.itemName
+    const brandName = req.body.brandName
+    const itemDescription = req.body.itemDescription
+    const quantity = req.body.quantity
+    const price = req.body.price
+
+    const item = new itemModel({ Name: itemName, Brand: brandName, Description: itemDescription, Quantity: quantity, Price: price });
 
     try {
 
         await item.save(); // Telling the database to save this information in the server
         res.send("Inserted data entry!")
-    } catch(err){
+    } catch (err) {
         console.log(err)
     }
 
 })
 
 // Starting the server
-app.listen(3001, () => {
+app.listen(process.env.PORT ?? 3001, () => {
     console.log('Server running on port 3001...')
 });
